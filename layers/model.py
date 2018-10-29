dcd58d1fb339443aa9c0401bad39ddeed1c57bee
isaactalx <154011252@qq.com>
2018-10-29 22:23:27 +0800

    添加anchor中心点位置、尺寸信息计算函数

--- layers/model.py
+++ layers/model.py
@@ -1,6 +1,8 @@
 import tensorflow.contrib.slim as slim
 import tensorflow as tf
 from layers.net_params import default_params
+import numpy as np
+import math
 
 
 def ssd_net(inputs,
@@ -9,6 +11,8 @@ def ssd_net(inputs,
             anchor_sizes=default_params.anchor_sizes,
             aspect_ratios=default_params.aspect_ratios,
             normalizations=default_params.normalizations,
+            is_training=True,
+            dropout_keep_prob=0.5,
             reuse=None,
             scope='ssd_300_vgg'):
     """
@@ -49,9 +53,11 @@ def ssd_net(inputs,
         # 外加的ssd层
         net = slim.conv2d(net, 1024, [3, 3], rate=6, scope='conv6')
         end_points['block6'] = net
+        net = tf.layers.dropout(net, rate=dropout_keep_prob, training=is_training)
 
         net = slim.conv2d(net, 1024, [1, 1], scope='conv7')
         end_points['block7'] = net
+        net = tf.layers.dropout(net, rate=dropout_keep_prob, training=is_training)
 
         with tf.variable_scope('block8'):
             net = slim.conv2d(net, 256, [1, 1], scope='conv1X1')
@@ -81,11 +87,11 @@ def ssd_net(inputs,
                                                     num_classes,
                                                     anchor_sizes[index],
                                                     aspect_ratios[index],
-                                                    normalizations=normalizations[i])
+                                                    normalizations=normalizations[index])
                 predictions.append(slim.softmax(cls_pred))
                 logits.append(cls_pred)
                 localizations.append(loc_pred)
-
+        # 返回类别预测结果，位置预测结果，所属某个类别概率值，特征层
         return predictions, localizations, logits, end_points
 
 
@@ -95,17 +101,18 @@ def multibox_layer(inputs,
                    aspect_ratios=[1],
                    normalizations=-1):
     """
+    生成anchor，代码中似乎并没有按照论文公式来计算
     这里默认输入格式为NHWC即[batch,height,width,channels]形式
-    :param inputs:
-    :param num_classes:
-    :param anchor_sizes:
-    :param aspect_ratios:
-    :param normalizations:
-    :param bn_normalization:
+    :param inputs:feature map
+    :param num_classes:类别(c+1)
+    :param anchor_sizes:[(min,max)...]
+    :param aspect_ratios:纵横比
+    :param normalizations:是否需要l2norm
     :return:
     """
     net = inputs
     if normalizations > 0:
+        # 第一个featuremap需要做一次l2正则，否则该层损失误差过大
         # TODO add l2normlization
         pass
     num_anchors = len(anchor_sizes) + len(aspect_ratios)  # 两个方形+ratios个长方形
@@ -132,3 +139,59 @@ def tensor_shape(x, rank=3):
         dynamic_shape = tf.unstack(tf.shape(x), rank)
         return [s if s is not None else d
                 for s, d in zip(static_shape, dynamic_shape)]
+
+
+def anchors_all_layers(img_shape, layers_shape, anchor_sizes, anchor_ratios, anchor_steps):
+    """
+    计算所有feature map的所有anchor信息
+    :param img_shape:
+    :param layers_shape:
+    :param anchor_sizes:
+    :param anchor_ratios:
+    :param anchor_steps:
+    :return:
+    """
+    layer_anchors = []  # 保存每个feature map的所有anchor信息
+    for i, feat_shape in enumerate(layers_shape):
+        anchors = anchors_one_layer(img_shape, feat_shape, anchor_sizes[i], anchor_ratios[i], anchor_steps[i])
+        layer_anchors.append(anchors)
+    return layer_anchors
+
+
+def anchors_one_layer(img_shape, feat_shape, sizes, ratios, step, offset=0.5, dtype=np.float32):
+    """
+    计算每个feature map的中心坐标,将其值归一化到原图和w,h并返回
+    :param img_shape:
+    :param feat_shape:
+    :param sizes:min & max
+    :param ratios:aspect ratios
+    :param step:特征图锚点框放大到原始图的缩放比例
+    :param offset:
+    :return:
+    """
+    y, x = np.mgrid[0:feat_shape[0], 0:feat_shape[1]]  # 对于block4[38*38],y=[[0,0,...,0],[1,1,...,1],...[37,37,...,37]]
+    y = (y.astype(dtype) + offset) * step / img_shape[0]
+    x = (x.astype(dtype) + offset) * step / img_shape[1]  # 这里返回的是中心点坐标与原图的相对位置(0-1)
+
+    y = np.expand_dims(y, axis=-1)
+    x = np.expand_dims(x, axis=-1)  # h*w*1
+
+    num_anchors = len(sizes) + len(ratios)  # 4,6,6,6,6,4
+
+    h = np.zeros((num_anchors,), dtype=dtype)
+    w = np.zeros((num_anchors,), dtype=dtype)
+
+    # min anchor的宽高/原图
+    h[0] = sizes[0] / img_shape[0]
+    w[0] = sizes[0] / img_shape[1]
+
+    di = 1  # anchor个数偏移
+    if len(sizes) > 1:
+        h[1] = math.sqrt(sizes[0] * sizes[1]) / img_shape[0]
+        w[1] = math.sqrt(sizes[0] * sizes[1]) / img_shape[1]
+        di += 1
+    for i, r in enumerate(ratios):
+        h[i + di] = sizes[0] / img_shape[0] / math.sqrt(r)
+        w[i + di] = sizes[0] / img_shape[1] * math.sqrt(r)
+    # 返回一个feature map中每个anchor中性点，size/原图尺寸,同一个cell中心点坐标一样
+    return y, x, h, w
--- layers/net_params.py
+++ layers/net_params.py
@@ -1,20 +1,17 @@
 from collections import namedtuple
 
-SSDParams = namedtuple('SSDParameters', ['img_shape',
-                                         'num_classes',
-                                         'no_annotation_label',
+SSDParams = namedtuple('SSDParameters', ['img_shape',  # 输入图片尺寸
+                                         'num_classes',  # 分类数
+                                         'no_annotation_label',  # 无标注标签
                                          'feat_layers',
                                          'feat_shapes',
                                          'anchor_size_bounds',
-                                         'anchor_sizes',
-                                         'aspect_ratios',
-                                         'anchor_steps',
-                                         'anchor_offset',
+                                         'anchor_sizes',  # 默认anchor大小
+                                         'aspect_ratios',  # anchor长宽比
+                                         'anchor_steps',  # 特征图相对原始图像的缩放
                                          'normalizations',
-                                         'prior_scaling'
+                                         'prior_scaling'  # 是对特征图参考框向gtbox做回归时用到的尺度缩放（0.1,0.1,0.2,0.2）
                                          ])
-
-
 
 
 def ssd300_config():
@@ -23,14 +20,16 @@ def ssd300_config():
     SSDParams.feat_layers = ['block4', 'block7', 'block8', 'block9', 'block10', 'block11']
     SSDParams.feat_shapes = [(38, 38), (19, 19), (10, 10), (5, 5), (3, 3), (1, 1)]
     SSDParams.anchor_sizes = [(21., 45.), (45., 99.), (99., 153.), (153., 207.), (207., 261.), (261., 315.)]
+    anchor_steps = [8, 16, 32, 64, 100, 300],  # 特征图锚点框放大到原始图的缩放比例
     SSDParams.aspect_ratios = [[2, .5],
                                [2, .5, 3, 1. / 3],
                                [2, .5, 3, 1. / 3],
                                [2, .5, 3, 1. / 3],
                                [2, .5],
                                [2, .5]]
-    SSDParams.normalizations=[20, -1, -1, -1, -1, -1]
+    SSDParams.normalizations = [20, -1, -1, -1, -1, -1]
     return SSDParams
     pass
 
-default_params=ssd300_config()
+
+default_params = ssd300_config()
